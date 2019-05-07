7e6c26332356a8eb28fbb460dd36624881982308
liujinrui <1342501157@qq.com>
2019-05-07 17:48:17 +0800

    wqe

--- application/common/Sms.php
+++ application/common/Sms.php
@@ -24,7 +24,7 @@ class Sms
 	*/
 	public function send($param){
 		$data = $this->checkdate($param);
-		print_r($data);die;
+		print_r($data);
 		$paramstring = http_build_query($data);
 		$content = $this->post($this->url, $paramstring);
 		$result = json_decode($content, true);
