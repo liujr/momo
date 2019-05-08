b6c06357721f2d8db46c255308ce3a3ccaab6ef7
liujinrui <1342501157@qq.com>
2019-05-08 17:42:10 +0800

    wq

--- application/common/Redis.php
+++ application/common/Redis.php
@@ -1,22 +1,61 @@
 <?php
 namespace app\common;
 use think\Config;
+use app\common\Common;
 
 /**
 * redis
 */
 class Redis
 {
+    public $redis = '';
+    /**
+     * 定义单例模式的变量
+     * @var null
+     */
+    private  static $_instance = null;
 
-	/**
-	* $key redis配置的键
-     * $str 自定义的字符串
-     * return key  返回redis键
-	*/
-	static public function getkey($key,$str='') {
-
-
+    public static function getInstance(){
+        if(empty(self::$_instance)){
+            self::$_instance = new self();
+        }
+        return self::$_instance;
     }
+
+    private function __construct(){
+        $this->redis = new \Redis();
+        $res = $this->redis->connect(config('redis.host'),config('redis.port'));
+        if($res === false){
+            Common::E('redis 连接失败');
+        }
+    }
+
+    /**
+     * 设置redis字符串
+     * @param $key 键
+     * @param $value值
+     * @param int $time过期时间
+     * @return bool|string
+     */
+    public function set($key,$value,$time = 0){
+        if(!$key) Common::E('key不能为空');
+        if(!$value) Common::E('value不能为空');
+        if(is_array($value)) $value = json_encode($value);
+        if(!$time) return $this->redis->set($key,$value);
+        return $this->redis->setex($key,$time,$value);
+    }
+
+    /**
+     * 获取redis值
+     * @param $key
+     * @return bool|string
+     */
+    public function get($key){
+        if(!$key) Common::E('key不能为空');
+        return $this->redis->get($key);
+    }
+
+
 
 }
 
---
+++ application/index/controller/Login.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+use app\common\Redis;
+use think\Exception;
+use app\common\Common;
+class Login
+{
+    /**
+     * 发送短信
+     */
+    public function index(){
+        try{
+            $code = intval($_GET['code']);
+            $mobile = intval($_GET['mobile']);
+            if(empty($code) ||  empty($mobile)) Common::E('电话号码或验证码不能为空');
+            $redisCode = Redis::getInstance()->get(config('redis.smskey').$mobile);
+            if($redisCode != $code) Common::E('验证码错误');
+            return Common::show(config('code.success'),'发送成功');
+        }catch (\Exception $e){
+            return Common::show(config('code.error'),$e->getMessage());
+        }
+
+    }
+}
