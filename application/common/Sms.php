4a112639d8579a6ae42fdb55f6fb62222b903352
liujinrui <1342501157@qq.com>
2019-05-08 09:00:39 +0800

    wqe

--- application/common/Sms.php
+++ application/common/Sms.php
@@ -13,7 +13,7 @@ class Sms
 	private $tplid = '';
 	public function __construct($type){
 		if(!$type)  Common::E('发送类型不能为空！');//return Common::show(config('code.error'),'');
-		if(!in_array($type,$this->tplidarr)) Common::E('发送类型不存在！');//return Common::show(config('code.error'),'');
+		if(!array_key_exists ($type,$this->tplidarr)) Common::E('发送类型不存在！');//return Common::show(config('code.error'),'');
 		$this->tplid = $this->tplidarr[$type];
 
 	}
