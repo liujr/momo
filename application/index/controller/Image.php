c55c7f3c486732c65c57e1f7124c1cdb964219ab
liujinrui <1342501157@qq.com>
2019-05-16 14:37:23 +0800

    we

--- application/index/controller/Image.php
+++ application/index/controller/Image.php
@@ -9,15 +9,14 @@ class Image
      */
     public function index(){
         try{
-            print_r($_FILES);
-           /*$file = request()->file('file');
+           $file = request()->file('file');
             $info = $file->move('../public/static/upload');
             if($info){
                 $data = ['img'=>config('url.host').'/upload/'.$info->getSaveName()];
                 return Common::show(config('code.success'),'上传成功',$data);
             }else{
                 return Common::show(config('code.error'),'上传失败');
-            }*/
+            }
         }catch (\Exception $e){
             return Common::show(config('code.error'),$e->getMessage());
         }
