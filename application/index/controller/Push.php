3a3c39bbba3e6096ec1cbfb5e15f3775ef8a8eb5
liujinrui <1342501157@qq.com>
2019-05-16 17:36:49 +0800

    11

---
+++ application/index/controller/Push.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+use think\Exception;
+use app\common\Common;
+class Push
+{
+    /**
+     * 发送短信
+     */
+    public function index(){
+        try{
+           print_r($_GET);
+            return Common::show(config('code.success'),'上传成功');
+        }catch (\Exception $e){
+            return Common::show(config('code.error'),$e->getMessage());
+        }
+
+    }
+}
