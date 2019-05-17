c62cf10acaaf348948c689195a51b7acbd07a9d9
liujinrui <1342501157@qq.com>
2019-05-17 11:09:12 +0800

    qwe

--- application/index/controller/Push.php
+++ application/index/controller/Push.php
@@ -10,9 +10,21 @@ class Push
      */
     public function index(){
         try{
+            if(empty($_GET))Common::E('请填入数据');
+            $teams = [
+                1=>['name'=>'马刺','logo'=>'/live/imgs/team1.png'],
+                4=>['name'=>'火箭','logo'=>'/live/imgs/team2.png'],
+            ];
+            $data = [
+                    'type' =>intval($_GET['type']),
+                    'title'  =>!empty($teams[$_GET['team_id']]['name']) ? $teams[$_GET['team_id']]['name']:'直播员',
+                     'logo'  =>!empty($teams[$_GET['team_id']]['logo']) ? $teams[$_GET['team_id']]['logo']:'',
+                     'content'  =>!empty($_GET['content']) ? $_GET['content']:'',
+                    'image'  =>!empty($_GET['image']) ? $_GET['image']:'',
+            ];
             $res = Redis::getInstance()->sMembers(config('redis.online_key'));
             foreach ($res as $k=>$v){
-                $_POST['http']->push($v, json_encode($_GET));
+                $_POST['http']->push($v, json_encode($data));
             }
             return Common::show(config('code.success'),'保存成功');
         }catch (\Exception $e){
--- public/static/live/js/live.js
+++ public/static/live/js/live.js
@@ -4,12 +4,13 @@ var socket = new WebSocket('ws://momo.mmrui.cn:8811');
 // 打开Socket
 socket.onopen = function(event) {
 // 发送一个初始化消息
-    console.log(event);
     console.log('success')
 }
 
 // 监听消息
 socket.onmessage = function(event) {
+
+    mypush(JSON.parse(event.data));
     console.log(event.data)
 };
 
@@ -17,3 +18,22 @@ socket.onmessage = function(event) {
 socket.onclose = function(event) {
     console.log('Client notified socket has closed',event);
 };
+
+function mypush(data){
+        var html = '<div class="frame">';
+                html  += '<h3 class="frame-header">';
+                html  += '<i class="icon iconfont icon-shijian"></i>第'+data.type+'节';
+                html  += '</h3>';
+                html  += '<div class="frame-item">';
+                html  += '<span class="frame-dot"></span>';
+                html  += '<div class="frame-item-author">';
+                if(data.logo){
+                    html  += '<img src="'+data.logo+'" width="20px" height="20px" />';
+                }
+                html +=data.title;
+                html  += '</div>';
+                html  += '<p>'+data.content+'</p>';
+                html  += '</div>';
+                html  += '</div>';
+                $('#match-result').prepend(html);
+}
