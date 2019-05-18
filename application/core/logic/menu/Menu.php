ff8cfc68afd296a6b961cbec368c8a2c8b7ffffd
liujinrui <1342501157@qq.com>
2019-05-18 15:15:29 +0800

    qw

--- application/core/logic/menu/Menu.php
+++ application/core/logic/menu/Menu.php
@@ -7,8 +7,8 @@ use app\common\Common;
 class Menu{
         public function lists(){
             $res =  Common::D('menu','MenuLists')->lists();
-            print_r($res);die;
             $data = Common::nodeMerge($res);
+            echo '<pre>';
             print_r($data);
         }
 }
