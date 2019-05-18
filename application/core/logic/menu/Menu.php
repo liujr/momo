3877dd761fc8a0e27eebb52c47eab4bddabc0423
liujinrui <1342501157@qq.com>
2019-05-18 15:11:57 +0800

    qw

--- application/core/logic/menu/Menu.php
+++ application/core/logic/menu/Menu.php
@@ -5,10 +5,8 @@
 namespace logic\menu;
 use app\common\Common;
 class Menu{
-        public function lists($pid){
-            if(empty($pid)) Common::E('电话号码不能为空');
-            $data = ['pid'=>$pid,'status'=>1];
-            $res =  Common::D('menu','MenuLists')->lists($data);
+        public function lists(){
+            $res =  Common::D('menu','MenuLists')->lists();
             $data = Common::nodeMerge($res);
             print_r($data);
         }
