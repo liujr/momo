2e087ea5cb13e2c0c4942ff84a14574d5e217577
liujinrui <1342501157@qq.com>
2019-05-18 16:49:04 +0800

    qw

--- public/static/home/js/layui/index.js
+++ public/static/home/js/layui/index.js
@@ -20,7 +20,6 @@ layui.use(['bodyTab','form','element','layer','jquery'],function(){
 		data = {pid:json};
 		$.post(url,data,function(res){
 
-			console.log(res.data);
             dataStr = res.data;
             //重新渲染左侧菜单
             tab.render(dataStr);
