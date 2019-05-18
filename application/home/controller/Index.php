ff94e33ca61bc567d883e74d9f1ad1366545f0b9
liujinrui <1342501157@qq.com>
2019-05-18 15:50:45 +0800

    qw

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -5,8 +5,6 @@ class Index extends Base {
     public function index() {
         $MenuObj = new \logic\menu\Menu();
         $data = $MenuObj->listsBypid();
-        echo '<pre>';
-        print_r($data);
         $this->assign(get_defined_vars());
         return $this->fetch();
     }
