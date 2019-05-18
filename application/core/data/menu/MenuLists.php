dfa88d652ee42f631079fc7f3dbee864d62c2d53
liujinrui <1342501157@qq.com>
2019-05-18 15:18:51 +0800

    qw

--- application/core/data/menu/MenuLists.php
+++ application/core/data/menu/MenuLists.php
@@ -7,8 +7,7 @@ class MenuLists{
 
     public function lists(){
         $where['status'] = 1;
-        $field = 'menuurl as href,menuname as title';
-        $info = Db::name('menu')->field($field)->where($where)->select();
+        $info = Db::name('menu')->where($where)->select();
         return $info;
     }
 }
