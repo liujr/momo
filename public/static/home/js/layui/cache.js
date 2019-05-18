04a216c36dcce8ca284d50ccb435d940079d002f
liujinrui <1342501157@qq.com>
2019-05-18 13:59:55 +0800

    qw

--- public/static/home/js/layui/cache.js
+++ public/static/home/js/layui/cache.js
@@ -5,7 +5,7 @@ layui.use(['form','jquery',"layer"],function() {
         $ = layui.jquery,
         layer = parent.layer === undefined ? layui.layer : top.layer;
 
-    
+
 
     //判断是否设置过头像，如果设置过则修改顶部、左侧和个人资料中的头像，否则使用默认头像
     if(window.sessionStorage.getItem('userFace') &&  $(".userAvatar").length > 0){
