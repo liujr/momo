b6ee38a28c23bc83354c4a65c5a9f8bc89e243aa
liujinrui <1342501157@qq.com>
2019-05-21 17:00:35 +0800

    ew

--- application/home/controller/Login.php
+++ application/home/controller/Login.php
@@ -3,9 +3,28 @@
 namespace app\home\controller;
 
 use \think\Controller;
+use app\common\Common;
+use app\common\Redis;
 class Login extends Controller{
 
+    /**
+     * 登录页面
+     * @return mixed
+     */
     public function index(){
         return $this->fetch();
+    }
+
+    public function getcode(){
+        try{
+            $account = input('account');
+            if(!$account) Common::E('用户名不能为空');
+            $randnum = Common::randnum(4,1);//随机获取4位数
+            Redis::getInstance()->set(config('config.admin_login').$account,$randnum,config('config.admin_login_out'));
+            return Common::show(config('code.success'),'发送成功',['code'=>$randnum]);
+        }catch (\Exception $e){
+            return Common::show(config('code.error'),$e->getMessage());
+        }
+       
     }
 }
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return[
+
+    'admin_login'=>'adminlogin',//后台登录键
+    'admin_login_out'=>60
+];
