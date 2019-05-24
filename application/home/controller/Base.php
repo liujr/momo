6253c484edc3ac41a3b1fe4acb08af538ca599f5
liujinrui <1342501157@qq.com>
2019-05-24 18:09:40 +0800

    qwe

--- application/home/controller/Base.php
+++ application/home/controller/Base.php
@@ -1,23 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/5/18
- * Time: 15:37
- */
+
 
 namespace app\home\controller;
 use \think\Controller;
 
 class Base extends Controller{
-    protected $request;
-    protected $app;
-    public function __construct(){
-        parent::__construct();
-        $this->checklogin();
-    }
 
-    public function checklogin(){
+
+    public function initialize(){
         $res = session(config('config.admin_user_login'));
         if(!$res) $this->redirect('/home/login/index');
     }
