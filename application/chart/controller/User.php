8a4b9301d2b2989a41bd816b5dddf66e1775e86e
liujinrui <1342501157@qq.com>
2019-05-24 16:21:33 +0800

    qwe

--- application/chart/controller/User.php
+++ application/chart/controller/User.php
@@ -12,6 +12,7 @@ class User extends Base{
             'avatar' =>'http://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg',
             'sex'     =>1
         ];
+        $this->assign(get_defined_vars());
         return $this->fetch();
     }
 
