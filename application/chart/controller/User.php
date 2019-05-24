1c41d20965f9a5954531e3773ef9bfb250177b48
liujinrui <1342501157@qq.com>
2019-05-24 16:26:17 +0800

    qwe

--- application/chart/controller/User.php
+++ application/chart/controller/User.php
@@ -11,7 +11,10 @@ class User extends Base{
             'sign'      =>'在深邃的编码世界，做一枚轻盈的纸飞机',
             'avatar' =>'http://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg',
             'sex'     =>1,
-            'age'    =>12
+            'age'    =>12,
+            'pid'    =>110000,
+            'cid'    =>110000,
+            'aid'    =>110101
         ];
         $this->assign(get_defined_vars());
         return $this->fetch();
