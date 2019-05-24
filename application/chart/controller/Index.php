53089e9fbc5d449d5227c9563e22a6b3eb15a194
liujinrui <1342501157@qq.com>
2019-05-24 11:31:07 +0800

    qwe

--- application/chart/controller/Index.php
+++ application/chart/controller/Index.php
@@ -4,6 +4,17 @@ namespace app\chart\controller;
 class Index extends Base{
 
     public function index(){
+        //聊天用户
+        $userInfo = [
+            'id' => 1,
+            'username' => 'admin',
+            'avatar' => 'admin',
+            'sign' => 'admin'
+        ];
+
+        $this->assign([
+            'uinfo' => $userInfo
+        ]);
         return $this->fetch();
     }
 
