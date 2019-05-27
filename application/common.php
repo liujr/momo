7402ad30cfe74fceac170200c1859985d6a4e10e
liujinrui <1342501157@qq.com>
2019-05-27 18:16:50 +0800

    ewq

--- application/common.php
+++ application/common.php
@@ -10,11 +10,5 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
-use app\common\Redis;
-use app\common\Common;
-function code(){
-    $randnum = Common::randnum(4,0);//随机获取4位数
-    $num = strtolower($randnum);
-    Redis::getInstance()->set(config('config.admin_login_code'),$num,config('config.admin_login_out'));
-    return $randnum;
-}
+
+
