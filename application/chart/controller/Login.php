037e75efa97605d36412b56672f9b3bb0cfd2f08
liujinrui <1342501157@qq.com>
2019-05-28 11:57:15 +0800

    qwe

--- application/chart/controller/Login.php
+++ application/chart/controller/Login.php
@@ -3,8 +3,18 @@ namespace app\chart\controller;
 use app\common\Redis;
 class Login extends Base{
 
+    /**
+     * 登录页面
+     */
      public function index(){
          return $this->fetch();
+    }
+
+    /**
+     * 注册页面
+     */
+    public function register(){
+        return $this->fetch();
     }
 
 
