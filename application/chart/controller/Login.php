cb7005b031ad1d2a0175e6bba692054f9d91f2dd
liujinrui <1342501157@qq.com>
2019-05-28 18:19:53 +0800

    qwe

--- application/chart/controller/Login.php
+++ application/chart/controller/Login.php
@@ -30,42 +30,6 @@ class Login extends Base{
         }catch (\Exception $e){
             Common::show(config('code.error'),$e->getMessage());
         }
-
-        if(request()->isAjax()){
-
-            $userName = input('post.user_name');
-            if(empty($userName)){
-                return json(['code' => -3, 'data' => '', 'msg' => '用户名不能为空']);
-            }
-
-            $pwd = input('post.pwd');
-            if(empty($pwd)){
-                return json(['code' => -4, 'data' => '', 'msg' => '密码不能为空']);
-            }
-
-            $user = db('chatuser')->field('id,user_name,pwd,sign,avatar')
-                ->where('user_name = "' . $userName . '"')->find();
-            if(empty($user)){
-                return json(['code' => -1, 'data' => '', 'msg' => '用户不存在']);
-            }
-
-            if( md5($pwd) != $user['pwd'] ){
-                return json(['code' => -2, 'data' => '', 'msg' => '密码错误']);
-            }
-
-            //设置用户登录
-            db('chatuser')->where('id', $user['id'])->setField('status', 1);
-
-            //设置session标识状态
-            session('f_user_name', $user['user_name']);
-            session('f_user_id', $user['id']);
-            session('f_user_sign', $user['sign']);
-            session('f_user_avatar', $user['avatar']);
-
-            return json(['code' => 1, 'data' => url('index/index'), 'msg' => '登录成功']);
-        }
-
-        $this->error('非法访问');
     }
 
     /**
