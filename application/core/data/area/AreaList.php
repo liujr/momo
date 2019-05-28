af98b1ad663bc65313484e3ca6e0733dc37a055f
liujinrui <1342501157@qq.com>
2019-05-28 17:39:37 +0800

    qwe

--- application/core/data/area/AreaList.php
+++ application/core/data/area/AreaList.php
@@ -6,7 +6,7 @@ class AreaList{
 
     public function getlist($param){
 
-        $where['id'] =['in',$param['ids']];
+        $where[] =['id','in',$param['ids']];
         $list = Db::name('area')->where($where)->select();
         $total  = Db::name('area')->where($where)->count();
         return [
