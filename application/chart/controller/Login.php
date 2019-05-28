7da249358c5b3f10307524f243324081765ca8e5
liujinrui <1342501157@qq.com>
2019-05-28 14:53:34 +0800

    qwe

--- application/chart/controller/Login.php
+++ application/chart/controller/Login.php
@@ -40,6 +40,63 @@ class Login extends Base{
         return $this->fetch();
     }
 
+    //处理注册
+    public function doRegister() {
+        if(request()->isAjax()){
+            $param = input('post.');
+
+            //TODO 理论上应该对所有的传入参数做正则校验,此处为了节省时间，暂时未做
+            if($param['pwd'] != $param['repwd']){
+                return json(['code' => -1, 'data' => '', 'msg' => '两次密码输入不一致']);
+            }
+
+            //查询获得区域描述
+            $where = 'id =' . $param['province'];
+            if(!empty($param['city'])){
+                $where .= ' or id=' . $param['city'];
+            }else{
+                $param['city'] = 0;
+            }
+
+            if(!empty($param['area'])){
+                $where .= ' or id=' . $param['area'];
+            }else{
+                $param['area'] = 0;
+            }
+            $area = db('area')->field('area_name')->where($where)->order('level asc')->select();
+
+            $areaStr = '';
+            if(!empty($area)){
+                foreach($area as $key=>$vo){
+                    $areaStr .= $vo['area_name'] . '-';
+                }
+                $areaStr = rtrim($areaStr, '-');
+            }else{
+                $areaStr = '北京-北京市-东城区';
+            }
+            unset($area);
+
+            $insertData = [
+                'user_name' => trim($param['user_name']),
+                'pwd' => md5($param['pwd']),
+                'sign' => '暂无',
+                'avatar' => config('avatar'),
+                'sex' => $param['sex'],
+                'age' => $param['age'],
+                'pid' => $param['province'],
+                'cid' => $param['city'],
+                'aid' => $param['area'],
+                'area' => $areaStr,
+                'status' => 0
+            ];
+            unset($param);
+
+            db('chatuser')->insert($insertData);
+            return json(['code' => '1', 'data' => url('login/index'), 'msg' => '注册成功']);
+        }
+        $this->error('非法访问');
+    }
+
 
 
 
--- public/static/common/js/register.js
+++ public/static/common/js/register.js
@@ -2,26 +2,13 @@ layui.use(['form', 'layer'], function () {
     var form = layui.form();
     var layer = layui.layer;
     form.verify({
-        username: function (value) {
-            if (Nick.GetLength(value) < 4 || Nick.GetLength(value) > 18) {
-                return '用户名必须在4-18个字符之间';
-            }
-
-            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
-                return '用户名不能有特殊字符';
-            }
-            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
-                return '用户名首尾不能出现下划线\'_\'';
-            }
-            if (/^\d+\d+\d$/.test(value)) {
-                return '用户名不能全为数字';
-            }
-        }
-        , pass: [
+         pass: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
         ]
     });
+
+
     //监听提交表单
     form.on('submit(*)', function (data) {
         layer.ready(function () {
