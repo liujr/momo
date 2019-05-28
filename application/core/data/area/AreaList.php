7a364806648493525e86a2197a9348709256b319
liujinrui <1342501157@qq.com>
2019-05-28 17:30:02 +0800

    qwe

--- application/core/data/area/AreaList.php
+++ application/core/data/area/AreaList.php
@@ -5,7 +5,8 @@ use think\Db;
 class AreaList{
 
     public function getlist($param){
-        if($param['ids']) $where['id'] =110100;
+
+        if($param['ids']) $where['id'] ='110100';
         $list = Db::name('area')->where($where)->select();
         echo Db::name('area')->fetchSql();die;
         $total = Db::name('area')->where($where)->count();
