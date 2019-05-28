a88d62e5185e367a79f4dd7744f881a4fb2628f2
liujinrui <1342501157@qq.com>
2019-05-28 11:17:08 +0800

    qwe

--- application/chart/controller/Base.php
+++ application/chart/controller/Base.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/5/24
- * Time: 11:16
- */
 
 namespace app\chart\controller;
 use \think\Controller;
 
 class Base extends Controller{
 
+    public function initialize(){
+        if(empty(session('username'))){
+            $this->redirect(url('/chart/login/index'));
+        }
+    }
 }
---
+++ application/chart/controller/Login.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\chart\controller;
+use app\common\Redis;
+class Login extends Base{
+
+     public function index(){
+         return $this->fetch();
+    }
+
+
+
+
+
+}
