9e3e32f930c56c163939ebe9f697c5fa4fb99bab
liujinrui <1342501157@qq.com>
2019-05-28 17:17:55 +0800

    qwe

--- application/core/data/area/AreaList.php
+++ application/core/data/area/AreaList.php
@@ -5,8 +5,9 @@ use think\Db;
 class AreaList{
 
     public function getlist($param){
-       /* if($param['ids']) $where['id'] =['in',];*/
-        $list = Db::name('area')->where('id','in', $param['ids'])->select();
+        $where = [];
+        if($param['ids']) $where = array_merage($where, ['id', 'in', $param['ids'] ]);
+        $list = Db::name('area')->where($where)->select();
         echo Db::name('area')->fetchSql();die;
         $total = Db::name('area')->where($where)->count();
         return [
