5206d48699638b7f8c26b53cca0908fa16f8025e
liujinrui <1342501157@qq.com>
2019-05-30 09:33:12 +0800

    we

--- application/core/logic/user/User.php
+++ application/core/logic/user/User.php
@@ -4,6 +4,7 @@ use app\common\Common;
 class User{
 
     public function lists($ids=0,$page=1,$limit=20){
+        echo $ids;die;
         $lists = Common::D('user','UserList')->lists($ids,$page,$limit);
         return $lists;
     }
