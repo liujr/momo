602cf940f501f658ce0dc2e38e639d99dc3b8ade
liujinrui <1342501157@qq.com>
2019-05-30 09:45:28 +0800

    we

--- application/core/data/user/UserList.php
+++ application/core/data/user/UserList.php
@@ -6,9 +6,9 @@ use think\Db;
 class UserList{
     public function lists($ids,$page=1,$limit=20){
 
-       
+
         $where['datastatus'] = 1;
-        $list = Db::name('user')->where('id','in',$ids)->where($where)->page($page)->limit($limit)->order('userid desc')->select();
+        $list = Db::name('user')->where('userid','in',$ids)->where($where)->page($page)->limit($limit)->order('userid desc')->select();
         echo Db::name('user')->fetchSql();die;
         $total = Db::name('user')->where($where)->count();
         return [
