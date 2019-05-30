f2b60f94a4cacbd599fca98c35eea00b293b245b
liujinrui <1342501157@qq.com>
2019-05-30 16:01:39 +0800

    we

--- application/core/data/friends/FriendsList.php
+++ application/core/data/friends/FriendsList.php
@@ -26,11 +26,11 @@ class FriendsList{
             ->alias('f')
             ->field(' u.userid as id,u.account as username,u.avatar,u.sign,u.is_online as status')
             ->join('me_user u ','f.friendid= u.userid')
-            ->where($where)->page($page)->limit($limit)->fetchSql(true)->select();
-        $total = Db::name('friends')->where($where)->fetchSql(true)->count();
+            ->where($where)->page($page)->limit($limit)->select();
+        $total = Db::name('friends')->where($where)->count();
         return [
-            'lists' => Db::query($list),
-            'total' =>Db::query($total),
+            'lists' => $list,
+            'total' =>$total,
             'page'  => $page,
             'limit' => $limit
         ];
