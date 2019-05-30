e5818e225aafb75192a23bcf218dafe7c6c59c64
liujinrui <1342501157@qq.com>
2019-05-30 09:58:20 +0800

    we

--- application/core/data/user/UserList.php
+++ application/core/data/user/UserList.php
@@ -7,7 +7,7 @@ class UserList{
     public function lists($ids,$page=1,$limit=20){
 
         if($ids) $where[] =['userid','not in',$ids];
-        $where['datastatus'] = 1;
+        $where[] = ['datastatus','=',1];
         $list = Db::name('user')->where($where)->page($page)->limit($limit)->order('userid desc')->select();
         //echo Db::name('user')->fetchSql();die;
         $total = Db::name('user')->where($where)->count();
