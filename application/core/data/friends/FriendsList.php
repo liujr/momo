d223c14327966434bdd360e45c3e95c251032268
liujinrui <1342501157@qq.com>
2019-05-31 10:30:55 +0800

    qw

--- application/core/data/friends/FriendsList.php
+++ application/core/data/friends/FriendsList.php
@@ -20,12 +20,12 @@ class FriendsList{
     public function getFriendsByCateId($userid,$cateid,$page=1,$limit=20){
         if(!$userid) Common::E('非法访问');
         if(!$cateid) Common::E('分类id不存在');
-        $list = Db::table('friends')
+        $list = Db::table('me_friends')
             ->alias(' f ')
             ->field(' u.userid as id,u.account as username,u.avatar,u.sign,u.is_online as status')
             ->join('me_user u ','f.friendid= u.userid')
             ->where("f.userid='$userid' and f.groupid='$cateid'")->page($page)->limit($limit)->select();
-        $total = Db::table('friends')->where("f.userid='$userid' and f.groupid='$cateid'")->count();
+        $total = Db::table('me_friends')->where("f.userid='$userid' and f.groupid='$cateid'")->count();
         return [
             'lists' => $list,
             'total' =>$total,
