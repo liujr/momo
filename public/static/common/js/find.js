4fad76f4de1ad754510ae0bd54ad7f6c38224d4e
liujinrui <1342501157@qq.com>
2019-06-05 15:01:31 +0800

    aa

--- public/static/common/js/find.js
+++ public/static/common/js/find.js
@@ -156,8 +156,9 @@ $(document).ready(function(){
             $.getJSON('/chart/group/ajaxlists', {'search_txt':search_txt}, function(res){
                 var _html = "";
                 $("#search_title").text('搜索结果：');
-                var res = eval("("+res+")");
+               // var res = eval("("+res+")");
                 if( 100 == res.code ){
+                    console.log(res.code);
                     console.log(res.code.tatal);
                     console.log(res.data.lists);
                     if(res.code.tatal ==0){
